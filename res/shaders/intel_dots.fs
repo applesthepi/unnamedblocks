670f293a9fb42a01827dc57c601943070ebc6063
The-Emperor10 <44948520+The-Emperor10@users.noreply.github.com>
2020-01-26 15:04:26 -0500

    fixed dots

--- res/shaders/intel_dots.fs
+++ res/shaders/intel_dots.fs
@@ -5,8 +5,8 @@ uniform vec3 dotColor;
 
 void main()
 {
-	if((mod(round(gl_FragCoord.x) + (planeInnerPosition.x * 2.0), dotMod) == 0.0 && mod(round(gl_FragCoord.y) + (planeInnerPosition.y * 2.0), dotMod * 2.0) == 0.0) ||
-		(mod(round(gl_FragCoord.x) + (dotMod / 2.0) + (planeInnerPosition.x * 2.0), dotMod) == 0.0 && mod(round(gl_FragCoord.y) + dotMod + (planeInnerPosition.y * 2.0), dotMod * 2.0) == 0.0))
+	if((mod(round(gl_FragCoord.x) + (planeInnerPosition.x * 2.0), dotMod) == 1.0 && mod(round(gl_FragCoord.y) + (planeInnerPosition.y * 2.0), dotMod * 2.0) == 1.0) ||
+		(mod(round(gl_FragCoord.x) + (dotMod / 2.0) + (planeInnerPosition.x * 2.0), dotMod) == 1.0 && mod(round(gl_FragCoord.y) + dotMod + (planeInnerPosition.y * 2.0), dotMod * 2.0) == 1.0))
 	{
 		gl_FragColor = vec4(dotColor.x, dotColor.y, dotColor.z, 1.0);
 	}
