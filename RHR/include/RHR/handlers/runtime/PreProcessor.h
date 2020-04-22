1a1b583b3472c3810f40180158777866d1e38836
The-Emperor10 <44948520+The-Emperor10@users.noreply.github.com>
2020-04-22 15:02:37 -0400

    Fixed linux build and cleaned out some commented cmake

--- RHR/include/RHR/handlers/runtime/PreProcessor.h
+++ RHR/include/RHR/handlers/runtime/PreProcessor.h
@@ -1,6 +1,6 @@
 #pragma once
+#include "config.h"
 #include "RHR/stacking/Plane.h"
-
 #include <mutex>
 #include <vector>
 #include <map>
--- VanillaMods/ub-ModVin/include/ModVin/Mod.h
+++ VanillaMods/ub-ModVin/include/ModVin/Mod.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "config.h"
 #include "ModCategories.h"
 
 #include "blocks/BlockPrint.h"
