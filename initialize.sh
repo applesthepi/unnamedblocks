f62163f52e1a3a714764055fdeebc53a63cb15b2
Jackson Oliver <applesthepi@gmail.com>
2021-07-27 15:41:14 -0400

    added linux auto config

---
+++ initialize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo test;
+git submodule update --init;
+./vcpkg/bootstrap-vcpkg.sh;
+./vcpkg/vcpkg upgrade;
+./vcpkg/vcpkg update;
+./vcpkg/vcpkg integrate install;
+./vcpkg/vcpkg install cpptoml:x64-linux;
+./vcpkg/vcpkg install freetype:x64-linux;
+./vcpkg/vcpkg install glad:x64-linux;
+./vcpkg/vcpkg install glfw3:x64-linux;
+./vcpkg/vcpkg install glm:x64-linux;
+./vcpkg/vcpkg install imgui:x64-linux;
+./vcpkg/vcpkg install rapidjson:x64-linux;
+./vcpkg/vcpkg install stb:x64-linux;
+./vcpkg/vcpkg install vulkan:x64-linux;
+./vcpkg/vcpkg install zstd:x64-linux;
+echo done;
