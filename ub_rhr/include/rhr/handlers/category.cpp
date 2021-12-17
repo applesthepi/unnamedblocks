b26e86f6145fb83d510bde986c650efc52cd5975
Jackson Oliver <applesthepi@gmail.com>
2021-12-17 12:45:24 -0500

    category ui changes and removed vendor imgui

--- ub_rhr/include/rhr/handlers/category.cpp
+++ ub_rhr/include/rhr/handlers/category.cpp
@@ -12,13 +12,13 @@
 
 static void button_callback_category(void* data)
 {
-	rhr::handler::category::active* active_catagory = (rhr::handler::category::active*)data;
+	rhr::handler::category::active* active_catagory = (rhr::handler::category::active*) data;
 	rhr::handler::category::select_category(*active_catagory);
 }
 
 static void button_callback_mod(void* data)
 {
-	usize* category_idx = (usize*)data;
+	usize* category_idx = (usize*) data;
 	rhr::handler::category::toggle_category(*category_idx);
 }
 
@@ -29,8 +29,10 @@ void rhr::handler::category::populate()
 //	m_render_frame->enable_background(cap::color::background_color_2);
 //	m_render_frame->disable_bar_movement();
 
-	const std::vector<rhr::registry::block::category_info>& category_infos = rhr::registry::block::get_registry().get_categories();
-	const std::vector<rhr::registry::block::block_info>& block_infos = rhr::registry::block::get_registry().get_blocks();
+	const std::vector<rhr::registry::block::category_info>& category_infos = rhr::registry::block::get_registry()
+		.get_categories();
+	const std::vector<rhr::registry::block::block_info>& block_infos = rhr::registry::block::get_registry()
+		.get_blocks();
 
 	//std::vector<std::string> binnedMods;
 	std::vector<std::vector<esp::mod::category*>> binned_catagories;
@@ -65,7 +67,7 @@ void rhr::handler::category::populate()
 				{
 					if (block_infos[b].block_mod_unlocalized_name == rhr::handler::project::mods[i] &&
 						block_infos[b].block_mod_block->get_category() ==
-						category_infos[a].category_mod_category->get_unlocalized_name())
+							category_infos[a].category_mod_category->get_unlocalized_name())
 					{
 						binned_blocks[i][ac].push_back(block_infos[b].block_mod_block);
 					}
@@ -84,10 +86,10 @@ void rhr::handler::category::populate()
 		usize* category_idx = new usize;
 		*category_idx = i;
 
- 		mod_group group = mod_group(cap::color::text_primary_color, cap::color::background_color_3);
+		mod_group group = mod_group(cap::color::text_primary_color, cap::color::background_color_3);
 
- 		group.id_mod = rhr::handler::project::mods[i];
- 		group.idx_mod = i;
+		group.id_mod = rhr::handler::project::mods[i];
+		group.idx_mod = i;
 
 // 		group.mod_category.reserve(binned_catagories[i].size());
 
@@ -118,14 +120,15 @@ void rhr::handler::category::populate()
 			active_catagories[a].mod_group = static_cast<u16>(i);
 			active_catagories[a].mod_group_category = static_cast<u16>(a);
 
-			std::shared_ptr<rhr::render::object::button_text> button = std::make_shared<rhr::render::object::button_text>(cap::color::black, cap::color::background_color_3,
-																														  binned_catagories[i][a]->get_display_name());
+			std::shared_ptr<rhr::render::object::button_text> button = std::make_shared<rhr::render::object::button_text>(
+				cap::color::black, cap::color::background_color_3,
+				binned_catagories[i][a]->get_display_name());
 			button->set_weak(button);
 			button->set_color_secondary(binned_catagories[i][a]->get_color());
 			button->enable_fill_width(true);
 			button->set_callback(button_callback_category, active_catagories + a);
-			button->set_size_local({ 200, 16 }, false);
-			button->set_position_local_physical({ 16, 0 }, false);
+			button->set_size_local({200, 16}, false);
+			button->set_position_local_physical({16, 0}, false);
 
 //			group.categories_render_frame->add_content(button, std::weak_ptr<rhr::render::interfaces::i_updateable>(), button, button, rhr::render::cardinal::local::DOWN);
 
@@ -141,15 +144,17 @@ void rhr::handler::category::populate()
 				std::shared_ptr<rhr::stack::collection> collection = std::make_shared<rhr::stack::collection>();
 				collection->set_weak(collection);
 				collection->display_vanity(false);
-				collection->set_position_local_physical({ 0, (rhr::stack::block::height + (rhr::stack::block::padding * 2)) * b });
+				collection->set_position_local_physical(
+					{0, (rhr::stack::block::height + (rhr::stack::block::padding * 2)) * b}
+				);
 
 				std::shared_ptr<rhr::stack::stack> stack = std::make_shared<rhr::stack::stack>();
 				stack->set_weak(stack);
 
 				std::shared_ptr<rhr::stack::block> block = std::make_shared<rhr::stack::block>(
-				binned_blocks[i][a][b]->get_unlocalized_name());
+					binned_blocks[i][a][b]->get_unlocalized_name());
 				block->set_weak(block);
-				
+
 				stack->add_block(block);
 				collection->add_stack(stack);
 
@@ -175,28 +180,48 @@ void rhr::handler::category::render()
 {
 	ImGui::Begin("mod_categories");
 
-	for (auto& group : m_mod_groups)
+	if (ImGui::CollapsingHeader("enabled mods"))
 	{
-		if (ImGui::Button(group.id_mod.c_str()))
-			group.enabled = !group.enabled;
+		for (auto& group: m_mod_groups)
+		{
+			std::string mod_cat_count = std::to_string(group.mod_category_ids.size()) + " categories";
+			ImGui::Selectable(group.id_mod.c_str(), &group.enabled);
+			ImGui::SameLine(100);
+			ImGui::Text(mod_cat_count.c_str());
+		}
+	}
 
-		if (group.enabled)
+	for (auto& group: m_mod_groups)
+	{
+		if (ImGui::CollapsingHeader(group.id_mod.c_str(), &group.enabled))
 		{
 			ImGui::Indent(15.0f);
 
 			for (u16 i = 0; i < static_cast<u16>(group.mod_category_ids.size()); i++)
 			{
-				auto button_color = group.mod_category_colors[i].get_normalized_scaled(0.5f, false);
-				ImGui::PushStyleColor(ImGuiCol_Button, { button_color.x, button_color.y, button_color.z, button_color.w });
+				auto category_color_100 = group.mod_category_colors[i].get_normalized();
+				auto category_color_10 = group.mod_category_colors[i].get_normalized_scaled(0.1f, false);
+
+				std::string cb_id = "cb_" + group.mod_category_ids[i];
+				ImGui::ColorButton(cb_id.c_str(), { category_color_100.x, category_color_100.y, category_color_100.z, category_color_100.w });
+
+				ImGui::SameLine();
+
+				ImGuiStyle& style = ImGui::GetStyle();
+				glm::vec<4, f32> base_color = { style.Colors[ImGuiCol_Button].x, style.Colors[ImGuiCol_Button].y, style.Colors[ImGuiCol_Button].z, style.Colors[ImGuiCol_Button].w };
+				base_color *= 0.8f;
+
+				ImGui::PushStyleColor(
+					ImGuiCol_Button, { category_color_10.x + base_color.x, category_color_10.y + base_color.y, category_color_10.z + base_color.z, category_color_10.w + base_color.w }
+				);
 
 				if (ImGui::Button(group.mod_category_ids[i].c_str()))
-					select_category({ group.idx_mod, i });
+					select_category({group.idx_mod, i});
 
 				ImGui::PopStyleColor();
 			}
 
 			ImGui::Unindent(15.0f);
-			ImGui::NewLine();
 		}
 	}
 
@@ -209,23 +234,29 @@ void rhr::handler::category::select_category(active active_category)
 
 	if (m_active_category.mod_group >= m_mod_groups.size())
 	{
-		cap::logger::warn(cap::logger::level::EDITOR,
+		cap::logger::warn(
+			cap::logger::level::EDITOR,
 			"Failed to load category. Mod idx out of range (" + std::to_string(m_active_category.mod_group) + " >= " +
-		std::to_string(m_mod_groups.size()) + ")");
+				std::to_string(m_mod_groups.size()) + ")"
+		);
 		return;
 	}
 
 	if (m_active_category.mod_group_category >= m_mod_groups[m_active_category.mod_group].mod_category_ids.size())
 	{
-		cap::logger::warn(cap::logger::level::EDITOR,
-			"Failed to load category. Category idx out of range (" + std::to_string(m_active_category.mod_group_category) +
-		" >= " + std::to_string(m_mod_groups[m_active_category.mod_group].mod_category_ids.size()) + ")");
+		cap::logger::warn(
+			cap::logger::level::EDITOR,
+			"Failed to load category. Category idx out of range (" +
+				std::to_string(m_active_category.mod_group_category) +
+				" >= " + std::to_string(m_mod_groups[m_active_category.mod_group].mod_category_ids.size()) + ")"
+		);
 		return;
 	}
 
 	rhr::stack::plane::toolbar_plane->delete_contents(true);
 
-	for (auto collection : m_mod_groups[m_active_category.mod_group].mod_category_collections[m_active_category.mod_group_category])
+	for (auto collection: m_mod_groups[m_active_category.mod_group].mod_category_collections[m_active_category
+		.mod_group_category])
 	{
 		collection->set_enabled(true);
 		rhr::stack::plane::toolbar_plane->add_collection(collection, false);
@@ -245,9 +276,9 @@ void rhr::handler::category::toggle_category(usize category_idx)
 
 void rhr::handler::category::reload_swap_chain()
 {
-	for (auto& mod_group : m_mod_groups)
+	for (auto& mod_group: m_mod_groups)
 		mod_group.reload_swap_chain();
 }
 
 std::vector<rhr::handler::category::mod_group> rhr::handler::category::m_mod_groups;
-rhr::handler::category::active rhr::handler::category::m_active_category = { 0, 0 };
+rhr::handler::category::active rhr::handler::category::m_active_category = {0, 0};
